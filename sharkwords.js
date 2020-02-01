76edfd0feadab3009e014d704b783c93ebc44781
Your Name <you@example.com>
2020-01-31 17:03:23 -0800

    Disabled letter buttons

--- sharkwords.js
+++ sharkwords.js
@@ -8,17 +8,22 @@ const WORDS = [
 let numWrong = 0;
 
 
-/** Loop over the chars in `word` and create divs. */
+//** Loop over the chars in `word` and create divs. */
 const createDivsForChars = (word) => {
   for (const char in word){
     $('#word-container').append(`<div class="letter-box ${char}"></div>`);
   }
+
+
 };
 
 
 /** Loop over each letter in `ALPHABET` and generate buttons. */
 const generateLetterButtons = () => {
-  // Replace this with your code
+  for (const letter of ALPHABET){
+    $('#letter-buttons').append(`<button>${letter}</button>`);
+  }
+
 };
 
 
@@ -27,13 +32,15 @@ const generateLetterButtons = () => {
  * `buttonEl` is an `HTMLElement` object.
  */
 const disableLetterButton = (buttonEl) => {
-  // Replace this with your code
+  const button = $(buttonEl);
+  button.attr('disabled', true);
 };
 
 
 /** Return `true` if `letter` is in the word. */
 const isLetterInWord = (letter) => {
-  // Replace this with your code
+  const elements = $(`div.${letter}`);
+  return (elements[0] === undefined);
 };
 
 
